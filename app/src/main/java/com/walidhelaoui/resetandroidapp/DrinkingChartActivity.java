8f5fe61f04db0ec800e027f48c03fc99cb600b83
Walid Helaoui <he.walid@gmail.com>
2017-12-11 23:20:46 +0100

    changing the charts (bar chart to the line chart)

--- app/src/main/java/com/walidhelaoui/resetandroidapp/DrinkingChartActivity.java
+++ app/src/main/java/com/walidhelaoui/resetandroidapp/DrinkingChartActivity.java
@@ -7,10 +7,11 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.walidhelaoui.resetandroidapp.Entity.DrinkingStatistics;
 import com.walidhelaoui.resetandroidapp.utils.DrinkingDataSource;
 
@@ -22,7 +23,7 @@ public class DrinkingChartActivity extends AppCompatActivity {
 
     private static final String TAG = "DrinkingChartActivity";
     private Handler mHandler;
-    BarChart chart;
+    LineChart chart;
     GifImageView loading;
     private Toolbar toolbar;
     private final Runnable m_Runnable = new Runnable()
@@ -33,22 +34,25 @@ public class DrinkingChartActivity extends AppCompatActivity {
             DrinkingChartActivity.this.mHandler.postDelayed(m_Runnable, 3000);
 
             if (!DrinkingDataSource.drinking.toString().isEmpty()){
-                chart = (BarChart) findViewById(R.id.chart);
                 chart.setVisibility(View.VISIBLE);
                 loading = (GifImageView) findViewById(R.id.loading);
                 loading.setVisibility(View.GONE);
                 Log.e(TAG, DrinkingDataSource.drinking.toString());
-                ArrayList<BarEntry> barEntries = new ArrayList<>();
+                ArrayList<Entry> entries = new ArrayList<>();
                 ArrayList<String> theDates = new ArrayList<>();
                 int i=0;
                 for(DrinkingStatistics statistics : DrinkingDataSource.drinking){
-                    barEntries.add(new BarEntry(Float.valueOf(statistics.getNumber()),i));
+                    entries.add(new Entry(Float.valueOf(statistics.getNumber()),i));
                     i++;
                     theDates.add("Day"+i);
                 }
-                BarDataSet barDataSet = new BarDataSet(barEntries,"Number of Cigarettes");
-                BarData data = new BarData(theDates,barDataSet);
+                LineDataSet lineDataSet = new LineDataSet(entries,"Number of Drinks");
+                LineData data = new LineData(theDates, lineDataSet);
+                lineDataSet.setColors(ColorTemplate.COLORFUL_COLORS); //
+                lineDataSet.setDrawCubic(true);
+                lineDataSet.setDrawFilled(true);
                 chart.setData(data);
+                chart.animateY(5000);
                 mHandler.removeCallbacksAndMessages(null);
             }
         }
@@ -60,7 +64,7 @@ public class DrinkingChartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinking_chart);
 
-        chart = (BarChart) findViewById(R.id.chart);
+        chart = (LineChart) findViewById(R.id.chart);
         chart.setVisibility(View.INVISIBLE);
         this.mHandler = new Handler();
         this.mHandler.postDelayed(m_Runnable,3000);
--- app/src/main/java/com/walidhelaoui/resetandroidapp/SmokingChartActivity.java
+++ app/src/main/java/com/walidhelaoui/resetandroidapp/SmokingChartActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.walidhelaoui.resetandroidapp.Entity.SmokingStatistics;
 import com.walidhelaoui.resetandroidapp.utils.SmokingDataSource;
 
@@ -22,7 +22,7 @@ import pl.droidsonroids.gif.GifImageView;
 public class SmokingChartActivity extends AppCompatActivity {
     private static final String TAG = "SmokingChartActivity";
     private Handler mHandler;
-    BarChart chart;
+    LineChart chart;
     GifImageView loading;
     private Toolbar toolbar;
     private final Runnable m_Runnable = new Runnable()
@@ -33,22 +33,25 @@ public class SmokingChartActivity extends AppCompatActivity {
             SmokingChartActivity.this.mHandler.postDelayed(m_Runnable, 3000);
 
             if (!SmokingDataSource.smoking.toString().isEmpty()){
-                chart = (BarChart) findViewById(R.id.chart);
                 chart.setVisibility(View.VISIBLE);
                 loading = (GifImageView) findViewById(R.id.loading);
                 loading.setVisibility(View.GONE);
                 Log.e(TAG, SmokingDataSource.smoking.toString());
-                ArrayList<BarEntry> barEntries = new ArrayList<>();
+                ArrayList<Entry> entries = new ArrayList<>();
                 ArrayList<String> theDates = new ArrayList<>();
                 int i=0;
                 for(SmokingStatistics statistics : SmokingDataSource.smoking){
-                    barEntries.add(new BarEntry(Float.valueOf(statistics.getNumber()),i));
+                    entries.add(new Entry(Float.valueOf(statistics.getNumber()),i));
                     i++;
                     theDates.add("Day"+i);
                 }
-                BarDataSet barDataSet = new BarDataSet(barEntries,"Number of Cigarettes");
-                BarData data = new BarData(theDates,barDataSet);
+                LineDataSet lineDataSet = new LineDataSet(entries,"Number of Cigarettes");
+                LineData data = new LineData(theDates, lineDataSet);
+                lineDataSet.setColors(ColorTemplate.COLORFUL_COLORS); //
+                lineDataSet.setDrawCubic(true);
+                lineDataSet.setDrawFilled(true);
                 chart.setData(data);
+                chart.animateY(5000);
                 mHandler.removeCallbacksAndMessages(null);
             }
         }
@@ -59,7 +62,7 @@ public class SmokingChartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smoking_chart);
 
-        chart = (BarChart) findViewById(R.id.chart);
+        chart = (LineChart) findViewById(R.id.chart);
         chart.setVisibility(View.INVISIBLE);
         this.mHandler = new Handler();
         this.mHandler.postDelayed(m_Runnable,3000);
