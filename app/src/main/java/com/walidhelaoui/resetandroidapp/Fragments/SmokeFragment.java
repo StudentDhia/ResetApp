0bf7c01e734adefadbad6dc7b1d98625513d73a6
Walid Helaoui <he.walid@gmail.com>
2017-12-28 16:41:27 +0100

    change the smoking menu

--- app/src/main/java/com/walidhelaoui/resetandroidapp/Fragments/SmokeFragment.java
+++ app/src/main/java/com/walidhelaoui/resetandroidapp/Fragments/SmokeFragment.java
@@ -5,11 +5,13 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,6 +38,9 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+import pl.bclogic.pulsator4droid.library.PulsatorLayout;
+import su.levenetc.android.badgeview.BadgeView;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -55,66 +60,108 @@ public class SmokeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_smoke, container, false);
 
-        final TextView savedMoney = (TextView) view.findViewById(R.id.savedMoney);
-        ValueAnimator animator = ValueAnimator.ofInt(0, smokeSavedMoney.getSavedMoney());
-        animator.setDuration(2000);
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            public void onAnimationUpdate(ValueAnimator animation) {
-                savedMoney.setText(animation.getAnimatedValue().toString());
-            }
-        });
-        animator.start();
 
-        ImageView savedMoneyImg = (ImageView) view.findViewById(R.id.savedMoneyImg);
-        savedMoneyImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getContext(),"hello",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(getActivity(), SmokingStatisticsActivity.class));
-            }
-        });
 
-        ImageView charts = (ImageView) view.findViewById(R.id.charts);
-        charts.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getActivity(), SmokingChartActivity.class));
-            }
-        });
-
-        ImageView quizImg = (ImageView) view.findViewById(R.id.quizImg);
-        quizImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getContext(),"aa",Toast.LENGTH_SHORT).show();
-                ((MainActivity)getActivity()).bottomBar.setVisibility(View.INVISIBLE);
-                ((MainActivity)getActivity()).replaceFragment(new DailyQuizS1Fragment());
-
-            }
-        });
-
-        ImageView missionsImg = (ImageView) view.findViewById(R.id.missionImg);
-        missionsImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((MainActivity)getActivity()).replaceFragment(new MissionsFragment());
-                ((MainActivity)getActivity()).bottomBar.setVisibility(View.INVISIBLE);
-            }
-        });
-
-        ImageView trophyImg = (ImageView) view.findViewById(R.id.trophyImg);
-        trophyImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((MainActivity)getActivity()).replaceFragment(new TrophysFragment());
-                ((MainActivity)getActivity()).bottomBar.setVisibility(View.INVISIBLE);
-            }
-        });
 
         return view;
 
 
 
     }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Inflate the layout for this fragment
+
+        final TextView welcome = (TextView) view.findViewById(R.id.Welcome);
+        final ImageView redbar1 = (ImageView) view.findViewById(R.id.redbar1);
+        final ImageView redbar2 = (ImageView) view.findViewById(R.id.redbar2);
+
+        final TextView welcome2 = (TextView) view.findViewById(R.id.Welcome2);
+        final ImageView redbar1A = (ImageView) view.findViewById(R.id.redbarA1);
+        final ImageView redbar2A = (ImageView) view.findViewById(R.id.redbarA2);
+
+        final TextView money = (TextView) view.findViewById(R.id.moneySaved);
+        money.setText(smokeSavedMoney.getSavedMoney()+"$");
+
+
+
+        final BadgeView badgeView = (BadgeView) view.findViewById(R.id.badgeMessage);
+
+        //money.setText(GETMONEYFROMDB);
+
+
+        PulsatorLayout pulsator = (PulsatorLayout) view.findViewById(R.id.pulsator);
+        pulsator.start();
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(5000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = welcome.getWidth();
+                final float translationX = width * progress;
+                welcome.setTranslationX(translationX);
+                welcome2.setTranslationX(translationX - width);
+            }
+        });
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = redbar1.getWidth();
+                final float translationX = width * progress;
+                redbar1.setTranslationX(-translationX);
+                redbar1A.setTranslationX(-translationX + width);
+            }
+        });
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = redbar2.getWidth();
+                final float translationX = width * progress;
+                redbar2.setTranslationX(-translationX);
+                redbar2A.setTranslationX(-translationX + width);
+            }
+        });
+        animator.start();
+
+        ImageView savedMoneyImg = (ImageView) view.findViewById(R.id.smoking_savedMoneyImg);
+        savedMoneyImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), SmokingStatisticsActivity.class));
+            }
+        });
+
+        ImageView charts = (ImageView) view.findViewById(R.id.smoking_charts);
+        charts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), SmokingChartActivity.class));
+
+            }
+        });
+
+        ImageView quizImg = (ImageView) view.findViewById(R.id.smoking_quizImg);
+        quizImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).replaceFragment(new DailyQuizS1Fragment());
+
+            }
+        });
+
+        new BadgeView.AnimationSet(badgeView).add("1", 2500).play();
+
+    }
+
 
 }
