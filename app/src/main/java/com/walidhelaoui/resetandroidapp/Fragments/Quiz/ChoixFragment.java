aa2f3cb29e2302a9ab649488cdd3e7324009cdab
Walid Helaoui <he.walid@gmail.com>
2017-12-28 16:56:22 +0100

    change ChoixFragment

--- app/src/main/java/com/walidhelaoui/resetandroidapp/Fragments/Quiz/ChoixFragment.java
+++ app/src/main/java/com/walidhelaoui/resetandroidapp/Fragments/Quiz/ChoixFragment.java
@@ -7,8 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 
+import com.appolica.flubber.Flubber;
 import com.walidhelaoui.resetandroidapp.Fragments.Quiz.DrinkingQuiz.QuizA1Fragment;
 import com.walidhelaoui.resetandroidapp.Fragments.Quiz.SmokingQuiz.QuizS1Fragment;
 import com.walidhelaoui.resetandroidapp.MainActivity;
@@ -19,7 +21,7 @@ import com.walidhelaoui.resetandroidapp.R;
  */
 public class ChoixFragment extends Fragment {
 
-    private ImageView cig,alchool;
+    private ImageView cig,alchool,arrow1,arrow2;
 
     public ChoixFragment() {
         // Required empty public constructor
@@ -33,12 +35,29 @@ public class ChoixFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_choix, container, false);
     }
 
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         cig = (ImageView) view.findViewById(R.id.cigarette);
         alchool = (ImageView) view.findViewById(R.id.alchool);
+        arrow1 = (ImageView) view.findViewById(R.id.quizArrow1);
+        arrow2 = (ImageView) view.findViewById(R.id.quizArrow2);
+
+        Flubber.with()
+                .animation(Flubber.AnimationPreset.SLIDE_RIGHT) // Slide up animation
+                .repeatCount(Animation.INFINITE)                              // Repeat once
+                .duration(1000)                              // Last for 1000 milliseconds(1 second)
+                .createFor(arrow1)                             // Apply it to the view
+                .start();                                    // Start it now
+
+        Flubber.with()
+                .animation(Flubber.AnimationPreset.SLIDE_LEFT) // Slide up animation
+                .repeatCount(Animation.INFINITE)                              // Repeat once
+                .duration(1000)                              // Last for 1000 milliseconds(1 second)
+                .createFor(arrow2)                             // Apply it to the view
+                .start();
 
         cig.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,5 +76,6 @@ public class ChoixFragment extends Fragment {
 
 
     }
+
 
 }
